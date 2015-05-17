963897f41408fe3dcae4be63646ffd347b4ac833
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2015-05-17 15:58:30 +0200

    Initial commit

---
+++ facebook.py
@@ -0,0 +1,42 @@
+import csv, json
+
+from bs4 import BeautifulSoup
+from progressbar import ProgressBar
+
+
+with open('config.json', 'r') as fd:
+    """ Contains personal name and is of format:
+         {
+             "name": "fubar"
+         }
+    """
+    config = json.load(fd)
+
+def clean_partner_info(s):
+    return s.replace(config['name'], '').replace(', ', '')
+
+print('Opening file')
+with open('facebook-kpjkpj/html/messages.htm', 'r') as fd:
+    soup = BeautifulSoup(fd.read())
+
+print('Loading content')
+content = soup.find('div', {'class': 'contents'})
+threads = content.find_all('div', {'class': 'thread'})
+
+with open('messages.csv', 'w') as fd:
+    cw = csv.writer(fd, quoting=csv.QUOTE_ALL)
+    cw.writerow(['partner', 'message_origin', 'date', 'message'])
+
+    pbar = ProgressBar(maxval=len(threads))
+    pbar.start()
+    for i, thread in enumerate(threads):
+        partner = clean_partner_info(thread.contents[0])
+
+        for msg in thread.find_all('div', {'class': 'message'}):
+            msg_ori = msg.find('span', {'class': 'user'}).text
+            meta = msg.find('span', {'class': 'meta'}).text
+            msg = msg.next_sibling.text
+
+            cw.writerow([partner, msg_ori, meta, msg])
+        pbar.update(i)
+    pbar.finish()
---
+++ wordcloud.R
@@ -0,0 +1,22 @@
+library(tm)
+library(wordcloud)
+
+
+messages <- read.csv("messages.csv", header=TRUE)
+
+corp <- Corpus(VectorSource(messages$message))
+corp <- tm_map(corp, stripWhitespace)
+corp <- tm_map(corp,  content_transformer(tolower))
+corp <- tm_map(corp, removeWords, stopwords("english"))
+corp <- tm_map(corp, removeWords, stopwords("german"))
+corp <- tm_map(corp, stemDocument)
+
+wordcloud(
+  corp,
+  scale=c(5,0.5),
+  #max.words=100,
+  random.order=FALSE,
+  rot.per=0.35,
+  use.r.layout=FALSE,
+  colors=brewer.pal(8, "Dark2")
+)
